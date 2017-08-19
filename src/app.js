9c164261dbde9d947ad1c78d2a1f4c07b564a3ed
arthurandrade <arthurandrade186@gmail.com>
2017-08-19 14:03:34 -0300

    added the module 'express-rate-limit' to limit repeated requests to public APIs and/or endpoint

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 const Express = require('express');
 
+const RateLimit = require('express-rate-limit');
+
 const healthCheckRouter = require('./routers/healthCheck');
 
 const app = new Express();
+
+var limiter = new RateLimit({
+    windowMs: 15*60*1000, // 15 minutes 
+    max: 5, // limit each IP to 100 requests per windowMs 
+    delayMs: 0 // disable delaying - full speed until the max limit is reached 
+  });
+
+ app.use(limiter);
 
 app.set('json spaces', 2);
 
